d6e7e6d7531f6699d63a36a2ca2152bbc63a14ee
rahulramanathan <rrahul1997@gmail.com>
2017-11-08 15:43:56 +0530

    Initial

---
+++ ManiPALS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipals;
+
+/**
+ *
+ * @author rahul
+ */
+public class ManiPALS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
